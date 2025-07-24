72203b0a90d5f7ee2cc802c972b2369160c7a4a4
Katsunobu1008 <katsunobu1008@gmail.com>
2025-07-25 00:39:31 +0900

    簡単なコードを書いた

---
+++ JavaStudy.java
@@ -0,0 +1,11 @@
+public class JavaStudy {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming!");
+
+        char ch;
+        ch = 'a';
+
+        System.out.println(ch);
+
+    }
+}
