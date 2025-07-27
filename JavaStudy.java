0178d1907c39d3614be106836efb7c1dd751efe0
Katsunobu1008 <katsunobu1008@gmail.com>
2025-07-27 23:48:26 +0900

    Section8 いろいろなメソッド

--- JavaStudy.java
+++ JavaStudy.java
@@ -16,11 +16,42 @@ public class JavaStudy {
         int number = 20;
         System.out.println(number);
 
-        int times = 100 + number;
+        int times = 100+ number;
         System.out.println(times * number);
 
+        int a = 4;
         int b = 8;
+        int numberOnePlayer = a * b;
+
         System.out.println(numberOnePlayer);
+
+        double pi = 3.14;
+        // 自動型変換を確かめる
+        System.out.println(pi * a*a);
+
+        // エスケープシーケンスについて
+        System.out.println("こんにちは\nJavaの世界へようこそ！");
+        System.out.println("こんにちは\nPythonの世界へようこそ！");
+
+        int n = 10;
+        n++;
+        System.out.println(n);
+        n--;
+        System.out.println(n);
+
+        int ans, s = 10;
+        ans = s++;
+
+        String msg = "こんにちは";
+        System.out.println("msgの値は" + msg + "です。");
+
+        System.out.println(Math.sqrt(pi));
+
+        double value = Math.random();
+        System.out.println("0.0以上1.0未満の乱数: " + value);
+
+        double value2 = Math.pow(2.5, 3);
+        System.out.println("2.5の3乗は: " + value2);
 
     }
 }
